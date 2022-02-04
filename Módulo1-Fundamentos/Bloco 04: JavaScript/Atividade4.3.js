5a9b66f0d35c1eacbd512d826e2eb1f9b4c3c2fb
GuilhermeAxhcar <guilhermeaxhcar@gmail.com>
2022-02-04 18:59:11 -0300

    Adiciona Atvidade 4 do bloco 4.3

--- Módulo1-Fundamentos/Bloco 04: JavaScript/Atividade4.3.js
+++ Módulo1-Fundamentos/Bloco 04: JavaScript/Atividade4.3.js
@@ -1,22 +1,25 @@
-//Atv1
-// let result = 1;
-// for(let count = 10; count > 1; count-=1){
-//     result = result *count;
-// }
-// console.log(result);
+/*
+Atv1
+let result = 1;
+for(let count = 10; count > 1; count-=1){
+    result = result *count;
+}
+console.log(result);
+------------------------------------------------------------------------------
 
-// Atv2
-// let word = 'trybe';
-// function inversor (str){
-//   let invertida = "";
-//   for (let count = str.length - 1; count >= 0; count-= 1){
-//      invertida += str[count];
-//   }
-//   return invertida;
-// }
-// console.log(inversor(word));
+Atv2
+let word = 'trybe';
+function inversor (str){
+  let invertida = "";
+  for (let count = str.length - 1; count >= 0; count-= 1){
+     invertida += str[count];
+  }
+  return invertida;
+}
+console.log(inversor(word));
+------------------------------------------------------------------------------
 
-//Atv3
+Atv3
 let array = ['java', 'javascript', 'python', 'html', 'css'];
 function encontraMaior (array){
   let posicaoMaior = 0;
@@ -43,3 +46,21 @@ function encontraMenor (array){
   return array[posicaoMenor];
 }
 console.log(encontraMenor(array));
+------------------------------------------------------------------------------
+*/
+Atv4
+let maiorPrimo = 0;
+
+for (let index = 2 ;index <= 50; index +=1){
+  let countDivisores = 0;  
+  for (let index2 = 1 ; index2 <= index/2 ; index2+=1){
+    if (index%index2==0){
+      countDivisores +=1;
+    }
+  }
+  if(countDivisores ==1){
+    maiorPrimo = index
+  }
+
+}
+console.log("O maior primo entre 1 e 50 é : "+maiorPrimo);
