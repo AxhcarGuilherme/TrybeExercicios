aed510a7b6d83f4ded09cbb2b5416aa962805d04
GuilhermeAxhcar <guilhermeaxhcar@gmail.com>
2022-02-04 16:31:41 -0300

    Atividade 3 do bloco 4.3

--- Módulo1-Fundamentos/Bloco 04: JavaScript/Atividade4.3.js
+++ Módulo1-Fundamentos/Bloco 04: JavaScript/Atividade4.3.js
@@ -6,14 +6,40 @@
 // console.log(result);
 
 // Atv2
-let word = 'trybe';
-function inversor (str){
-  let invertida = "";
-  for (let count = str.length - 1; count >= 0; count-= 1){
-     invertida += str[count];
-  }
-  return invertida;
-}
-console.log(inversor(word));
+// let word = 'trybe';
+// function inversor (str){
+//   let invertida = "";
+//   for (let count = str.length - 1; count >= 0; count-= 1){
+//      invertida += str[count];
+//   }
+//   return invertida;
+// }
+// console.log(inversor(word));
 
 //Atv3
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+function encontraMaior (array){
+  let posicaoMaior = 0;
+  let numCaracteres = 0;
+  for (let count = 0; count < array.length; count +=1) {
+    if(array[count].length > numCaracteres){
+      posicaoMaior = count;
+      numCaracteres = array[count].length;
+    }
+  }
+  return array[posicaoMaior];
+}
+console.log(encontraMaior(array));
+
+function encontraMenor (array){
+  let posicaoMenor = 0;
+  let numCaracteres = 99999;
+  for (let count = 0; count < array.length; count +=1) {
+    if(array[count].length < numCaracteres){
+      posicaoMenor = count;
+      numCaracteres = array[count].length;
+    }
+  }
+  return array[posicaoMenor];
+}
+console.log(encontraMenor(array));
