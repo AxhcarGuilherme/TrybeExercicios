4dc01250fb9664bbc23ed6ca83c5a0b54b5a84ba
GuilhermeAxhcar <guilhermeaxhcar@gmail.com>
2022-02-04 16:06:36 -0300

    Atividade 2 bloco 4.3

--- Módulo1-Fundamentos/Bloco 04: JavaScript/Atividade4.3.js
+++ Módulo1-Fundamentos/Bloco 04: JavaScript/Atividade4.3.js
@@ -1,6 +1,11 @@
 //Atv1
-let result = 1;
-for(let count = 10; count > 1; count-=1){
-    result = result *count;
-}
-console.log(result);
+// let result = 1;
+// for(let count = 10; count > 1; count-=1){
+//     result = result *count;
+// }
+// console.log(result);
+
+// Atv2
+let word = 'trybe';
+let invertida  = word.split("").reverse("").join("");
+console.log(invertida);
