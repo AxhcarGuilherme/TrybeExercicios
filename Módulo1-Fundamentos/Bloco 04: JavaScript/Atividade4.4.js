820f88250b4ca8c6827fbf0099af8e179cdfcb4e
GuilhermeAxhcar <guilhermeaxhcar@gmail.com>
2022-02-07 15:17:31 -0300

    Adiciona atividades sobre objetos e for/in

---
+++ Módulo1-Fundamentos/Bloco 04: JavaScript/Atividade4.4.js
@@ -0,0 +1,122 @@
+// Atividade sobre objetos e for/in
+{
+  let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos   quadrinhos do Pato Donald',
+    recorrente: 'Sim',
+  }
+
+  // Atividade 1
+  {
+  // console.log("Bem-vinda, "+info.personagem);  
+  }
+  // ------------------------------------------------
+  
+  // Atividade 2
+  {
+  // console.log(info);
+  }
+  // ------------------------------------------------
+  
+  // Atividade 3
+  {
+  // for (let key  in info) {
+  //   console.log(key);
+  // }
+  }
+  // ------------------------------------------------
+
+  // Atividade 4
+  {
+  // for (let key  in info) {
+  //   console.log(info[key]);
+  // }
+  }
+  // ------------------------------------------------
+
+  // Atividae 5
+  {
+  //   let infoPato = {
+  //     personagem: 'Tio Patinhas',
+  //     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  //     nota: 'O último MacPatinhas', 
+  //     recorrente: 'Sim',
+  //   }
+  //   for (let key  in info && infoPato) {
+  //     if (key == 'recorrente') {
+  //       if(info[key] == 'Sim' && infoPato[key]=='Sim'){
+  //         console.log("Ambos recorrentes");
+  //       }
+  //     }
+  //     else {
+  //       console.log(info[key] +' e '+infoPato[key]);
+  //     }
+    
+  //   }
+  }
+  // ------------------------------------------------
+
+  // Atividade 6
+  {
+    // let leitor = {
+    //   nome: 'Julia',
+    //   sobrenome: 'Pessoa',
+    //   idade: 21,
+    //   livrosFavoritos: [
+    //     {
+    //       titulo: 'O Pior Dia de Todos',
+    //       autor: 'Daniela Kopsch',
+    //       editora: 'Tordesilhas',
+    //     },
+    //   ],
+    // }
+    // console.log("O livro favorito de "+ leitor.nome +' '+leitor.sobrenome+' se chama '+ leitor.livrosFavoritos[0].titulo);
+  }
+  // ------------------------------------------------
+
+  // Atividade 7
+  {
+    // let leitor = {
+    //   nome: 'Julia',
+    //   sobrenome: 'Pessoa',
+    //   idade: 21,
+    //   livrosFavoritos: [
+    //     {
+    //       titulo: 'O Pior Dia de Todos',
+    //       autor: 'Daniela Kopsch',
+    //       editora: 'Tordesilhas',
+    //     },
+    //     {
+    //       titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    //       autor: 'JK Rowling',
+    //       editora: 'Rocco',
+    //     }
+    //   ],
+    // }
+  }
+  // ------------------------------------------------
+
+  // Atividade 8
+  // let leitor = {
+  //   nome: 'Julia',
+  //   sobrenome: 'Pessoa',
+  //   idade: 21,
+  //   livrosFavoritos: [
+  //     {
+  //       titulo: 'O Pior Dia de Todos',
+  //       autor: 'Daniela Kopsch',
+  //       editora: 'Tordesilhas',
+  //     },
+  //     {
+  //       titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  //       autor: 'JK Rowling',
+  //       editora: 'Rocco',
+  //     }
+  //   ],
+  // }
+  // let count = 0;
+  // count = leitor.livrosFavoritos.length;
+  // console.log(leitor.nome +' tem '+ count+' Livros favoritos' );
+
+}
