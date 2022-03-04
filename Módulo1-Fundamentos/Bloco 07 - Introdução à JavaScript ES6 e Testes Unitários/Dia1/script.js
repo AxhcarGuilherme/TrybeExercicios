b21dc430d3f153f61f7b5e5ab0de68fba7a2fb6e
GuilhermeAxhcar <guilhermeaxhcar@gmail.com>
2022-03-03 21:30:12 -0300

    Atualiza atividade do bloco 7.1

--- Módulo1-Fundamentos/Bloco 07 - Introdução à JavaScript ES6 e Testes Unitários/Dia1/script.js
+++ Módulo1-Fundamentos/Bloco 07 - Introdução à JavaScript ES6 e Testes Unitários/Dia1/script.js
@@ -1,22 +1,54 @@
-const testingScope = (escopo) => {
-  if (escopo === true) {
-    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = `${ifScope} ótimo, fui utilizada no escopo!`;
-    console.log(ifScope);
-  } else {
-    var elseScope = `Não devo ser utilizada fora meu escopo (else)`;
-    console.log(elseScope);
-  }
-  // console.log(ifScope + ' o que estou fazendo aqui ? :O');
-}
+// Parte 1
+// Atividade 1 --------------------------------------------
+// const testingScope = (escopo) => {
+//   if (escopo === true) {
+//     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = `${ifScope} ótimo, fui utilizada no escopo!`;
+//     console.log(ifScope);
+//   } else {
+//     var elseScope = `Não devo ser utilizada fora meu escopo (else)`;
+//     console.log(elseScope);
+//   }
+//   // console.log(ifScope + ' o que estou fazendo aqui ? :O');
+// }
 
-testingScope(true);
+// testingScope(true);
 
-// Atividade 2
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-const orderList = (array) => array.sort((a, b) => a - b);
-const list = orderList(oddsAndEvens);
-console.log(`Os números ${list} se encontram ordenados de forma crescente`);
+// Atividade 2 --------------------------------------------
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+// const orderList = (array) => array.sort((a, b) => a - b);
+// const list = orderList(oddsAndEvens);
+// console.log(`Os números ${list} se encontram ordenados de forma crescente`);
 
 // Parte2
+// Atividade 1 --------------------------------------------
+// const fatorial = (numero) => numero < 2 ? 1 : numero * fatorial(numero-1);
+// console.log(fatorial(5));
 
+
+// Atividade 2 --------------------------------------------
+// const longestWord = phrase => {
+//   let wordList = phrase.split(' ');
+//   let longestSize = 0;
+//   let wordResult = '';
+//   for (const word of wordList) {
+//     if (word.length > longestSize) {
+//       longestSize = word.length;
+//       wordResult = word;
+//     }
+//   }
+//   return wordResult;
+// }
+// console.log(longestWord('Antonio foi no banheiro e não sabemos o que aconteceu'));
+
+// Atividade 3 --------------------------------------------
+// const buttonCount = document.getElementById('button');
+// const count = document.getElementById('click-count');
+// let number = 1;
+// const addCount = () => {
+//   count.innerHTML = number;
+//   number += 1;
+
+// }
+
+// buttonCount.addEventListener('click', addCount);
